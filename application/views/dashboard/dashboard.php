b2c9c4d4819d0b25a0e2dd11f99335e23da5ed38
ANDI MUHAMMAD REZKY M <andimuhammadrezkym@gmail.com>
2022-12-11 23:48:21 +0800

    penambahan kalender

--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -99,7 +99,72 @@
                             </div>
                         </div>
                     </a>
-                </div>    
+                </div>
+                <div class="main__calendar">
+                    <div class="calendar__Container">
+                        <div class="calendar__title">
+                            Calendar
+                        </div>
+                        <div class="calendar__header">
+                            <p class="calendar__currentdate">
+                                December 2022
+                            </p>
+                            <div class="calendar__icon">
+                                <i class="fa-solid fa-chevron-left"></i>
+                                <i class="fa-solid fa-chevron-right"></i>
+                            </div>
+                        </div>
+                        <div class="calendar__items">
+                            <ul class="calendar__weeks">
+                                <li>Sun</li>
+                                <li>Mon</li>
+                                <li>Tue</li>
+                                <li>Wed</li>
+                                <li>Thu</li>
+                                <li>Fri</li>
+                                <li>Sat</li>
+                            </ul>
+                            <ul class="calendar__days">
+                                <li class="--inactive">27</li>
+                                <li class="--inactive">28</li>
+                                <li class="--inactive">29</li>
+                                <li class="--inactive">30</li>
+                                <li>1</li>
+                                <li>2</li>
+                                <li>3</li>
+                                <li>4</li>
+                                <li>5</li>
+                                <li>6</li>
+                                <li>7</li>
+                                <li>8</li>
+                                <li class="active">9</li>
+                                <li>10</li>
+                                <li>11</li>
+                                <li>12</li>
+                                <li>13</li>
+                                <li>14</li>
+                                <li>15</li>
+                                <li>16</li>
+                                <li>17</li>
+                                <li>18</li>
+                                <li>19</li>
+                                <li>20</li>
+                                <li>21</li>
+                                <li>22</li>
+                                <li>23</li>
+                                <li>24</li>
+                                <li>25</li>
+                                <li>26</li>
+                                <li>27</li>
+                                <li>28</li>
+                                <li>29</li>
+                                <li>30</li>
+                                <li>31</li>
+                                <li class="--inactive">1</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>     
             </div>
         </div>
     </div>
